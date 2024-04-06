103c69adfb7b85035b112acb02f28ae9a5ac5ee3
Marcello <hey@marcello.codes>
2024-04-05 20:08:12 -0400

    Revert "feature: monetize"
    This reverts commit f5c6213cfd41ad270b2ac1f52820655c5a8354ff.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2044180719717177"
           crossOrigin="anonymous"
         ></script>
-        <script
-          src="https://dibsemey.com/pfe/current/tag.min.js?z=7315429"
-          data-cfasync="false"
-          async
-        ></script>
         <meta name="monetag" content="e6af2b0dc39220485e7231c64ea8a005"></meta>
       </head>
       <body className="pt-[66px]">
