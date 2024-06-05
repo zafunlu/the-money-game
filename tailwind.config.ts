32eda2efd64677b44e37aa9bcd33dda1c3969caf
Zafer Unlu <zafer.unlu@coolblue.nl>
2024-06-05 18:07:02 +0200

    Changed font to Inter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       container: {
         padding: "12px",
         center: true,
