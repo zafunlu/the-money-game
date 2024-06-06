6d66356c0005a51e0a5aec100c78093e416f76de
Zafer Unlu <zafer.unlu@coolblue.nl>
2024-06-06 08:18:40 +0200

    Correct URL

--- app/constants/app-constants.tsx
+++ app/constants/app-constants.tsx
@@ -1,4 +1,5 @@
 export const AppConstants = {
   BACKEND_URL: process.env.NEXT_PUBLIC_API_URL || "https://api.fun-banking.com",
-  APP_URL: process.env.NEXT_PUBLIC_APP_URL || "localhost:3000",
+  APP_URL:
+    process.env.NEXT_PUBLIC_APP_URL || "https://the-money-game.vercel.app/",
 };
